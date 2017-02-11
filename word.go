0cb0de9de877dd562990875e7889461a1c0c1cd5
Yasuhisa Yoshida <syou6162@gmail.com>
2017-02-12 03:31:55 +0900

    子要素のポインタ持たせる

--- word.go
+++ word.go
@@ -1,16 +1,17 @@
 package main
 
 type Word struct {
-	surface string
-	lemma   string
-	posTag  string
-	cposTag string
-	idx     int
-	head    int
+	surface  string
+	lemma    string
+	posTag   string
+	cposTag  string
+	idx      int
+	head     int
+	children []*Word
 }
 
 func makeWord(surface string, posTag string, idx int, head int) *Word {
-	return &Word{surface, surface, posTag, posTag, idx, head}
+	return &Word{surface, surface, posTag, posTag, idx, head, make([]*Word, 0)}
 }
 
 func makeRootWord() *Word {
