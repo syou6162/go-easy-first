c096aceddb1bf43b6ccc05e212573e1fd94c2d02
Yasuhisa Yoshida <syou6162@gmail.com>
2017-02-12 17:42:17 +0900

    pendingから削除する関数を追加

--- state.go
+++ state.go
@@ -4,3 +4,8 @@ type State struct {
 	pending []*Word
 	arcs    map[int]int
 }
+
+func (state *State) deletePending(idx int) []*Word {
+	state.pending = append(state.pending[:idx], state.pending[idx+1:]...)
+	return state.pending
+}
