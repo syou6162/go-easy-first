62adcf9fb0821dcd9319909db7840870e5953cd1
Yasuhisa Yoshida <syou6162@gmail.com>
2017-02-12 19:20:31 +0900

    EdgeForを追加

--- perceptron.go
+++ perceptron.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 // GoldArcs returns map of parent => children
 func GoldArcs(sent *Sentence) map[int][]int {
 	result := make(map[int][]int)
@@ -12,4 +14,16 @@ func GoldArcs(sent *Sentence) map[int][]int {
 		}
 	}
 	return result
+}
+
+// EdgeFor returns a pair of parent index and child index
+func EdgeFor(state *State, actionID int, idx int) ([]int, error) {
+	switch actionID {
+	case 0:
+		return []int{state.pending[idx].idx, state.pending[idx+1].idx}, nil
+	case 1:
+		return []int{state.pending[idx+1].idx, state.pending[idx].idx}, nil
+	default:
+		return nil, errors.New("Invalid line")
+	}
 }
