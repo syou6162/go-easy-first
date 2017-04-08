a907f284483b8d317a97e0bb8fe098345e422cd1
Yasuhisa Yoshida <syou6162@gmail.com>
2017-04-09 03:30:18 +0900

    モデルの更新関数を追加

--- perceptron_test.go
+++ perceptron_test.go
@@ -99,3 +99,38 @@ func TestCandidateActions(t *testing.T) {
 		t.Error("length of candidate actions must be 10")
 	}
 }
+
+func TestUpdateWeight(t *testing.T) {
+	model := Model{make(map[string]float64), make(map[string]float64), 1}
+	gold := []string{"1", "2", "3"}
+	predict := []string{"1", "3", "4"}
+	model.updateWeight(&gold, &predict)
+
+	if w, _ := model.weight["1"]; w != 0 {
+		t.Error("weight of '1' must be 0")
+	}
+	if w, _ := model.weight["2"]; w != 1 {
+		t.Error("weight of '2' must be 1")
+	}
+	if w, _ := model.weight["3"]; w != 0 {
+		t.Error("weight of '3' must be 0")
+	}
+	if w, _ := model.weight["4"]; w != -1 {
+		t.Error("weight of '4' must be -1")
+	}
+
+	model.updateWeight(&gold, &predict)
+
+	if w, _ := model.cumWeight["1"]; w != 0 {
+		t.Error("cumWeight of '1' must be 0")
+	}
+	if w, _ := model.cumWeight["2"]; w != 3 {
+		t.Error("cumWeight of '2' must be 3")
+	}
+	if w, _ := model.cumWeight["3"]; w != 0 {
+		t.Error("cumWeight of '3' must be 0")
+	}
+	if w, _ := model.cumWeight["4"]; w != -3 {
+		t.Error("cumWeight of '4' must be -3")
+	}
+}
