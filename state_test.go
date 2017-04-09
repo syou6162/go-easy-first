1a69dd885cd78b07db642523c848dd74a65372e1
Yasuhisa Yoshida <syou6162@gmail.com>
2017-04-09 20:41:51 +0900

    deletePendingのテストを追加

---
+++ state_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestDeletePending(t *testing.T) {
+	words := make([]*Word, 0)
+	words = append(words,
+		makeRootWord(),
+		makeWord("ms.", "NNP", 0, -1),
+		makeWord("hang", "NNP", 1, 0),
+		makeWord("plays", "VBZ", 2, 1),
+	)
+	s := &State{words, make(map[int]int)}
+	s.deletePending(2)
+
+	if s.pending[1].surface != "ms." {
+		t.Error("surface must be 'ms.'")
+	}
+	if s.pending[2].surface != "plays" {
+		t.Error("surface must be 'plays'")
+	}
+
+	s.deletePending(1)
+	if s.pending[1].surface != "plays" {
+		t.Error("surface must be 'plays'")
+	}
+}
