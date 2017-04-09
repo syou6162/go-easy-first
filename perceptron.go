47fe5fd05ec69507e13a15b863fbb18390d48949
Yasuhisa Yoshida <syou6162@gmail.com>
2017-04-09 15:37:18 +0900

    headがポインタである必要は特になかった

--- perceptron.go
+++ perceptron.go
@@ -9,7 +9,7 @@ import (
 func GoldArcs(sent *Sentence) map[int][]int {
 	result := make(map[int][]int)
 	for idx, w := range sent.words {
-		head := *w.head
+		head := w.head
 		if children, ok := result[head]; ok {
 			result[head] = append(children, idx)
 		} else {
