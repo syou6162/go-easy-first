2aefc53cbca19392e4c965c40716a0bdbb4531f2
Yasuhisa Yoshida <syou6162@gmail.com>
2017-04-11 21:30:01 +0900

    action名を素性にちゃんと付与する

--- feature.go
+++ feature.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"math"
 	"reflect"
+	"runtime"
 )
 
 func NilSafePosTag(w *Word) string {
@@ -15,7 +15,7 @@ func NilSafePosTag(w *Word) string {
 	return posTag
 }
 
-func addUnigramFeatures(features *[]string, state *State, idx int, prefix string) {
+func addUnigramFeatures(features *[]string, state *State, actName string, idx int, prefix string) {
 	if idx < 0 || idx >= len(state.pending) {
 		return
 	}
@@ -23,13 +23,13 @@ func addUnigramFeatures(features *[]string, state *State, idx int, prefix string
 	lcp := NilSafePosTag(w.LeftMostChild())
 	rcp := NilSafePosTag(w.RightMostChild())
 	*features = append(*features,
-		fmt.Sprintf("%s+surface:%s", prefix, w.surface),
-		fmt.Sprintf("%s+lemma:%s", prefix, w.lemma),
-		fmt.Sprintf("%s+posTag:%s", prefix, w.posTag),
-		fmt.Sprintf("%s+cposTag:%s", prefix, w.cposTag),
-		fmt.Sprintf("%s+posTag:%s+leftmost:%s", prefix, w.posTag, lcp),
-		fmt.Sprintf("%s+posTag:%s+rightmost:%s", prefix, w.posTag, rcp),
-		fmt.Sprintf("%s+posTag:%s+leftmost:%s+rightmost:%s", prefix, w.posTag, lcp, rcp),
+		fmt.Sprintf("%s+%s+surface:%s", actName, prefix, w.surface),
+		fmt.Sprintf("%s+%s+lemma:%s", actName, prefix, w.lemma),
+		fmt.Sprintf("%s+%s+posTag:%s", actName, prefix, w.posTag),
+		fmt.Sprintf("%s+%s+cposTag:%s", actName, prefix, w.cposTag),
+		fmt.Sprintf("%s+%s+posTag:%s+leftmost:%s", actName, prefix, w.posTag, lcp),
+		fmt.Sprintf("%s+%s+posTag:%s+rightmost:%s", actName, prefix, w.posTag, rcp),
+		fmt.Sprintf("%s+%s+posTag:%s+leftmost:%s+rightmost:%s", actName, prefix, w.posTag, lcp, rcp),
 	)
 }
 
@@ -72,41 +72,25 @@ func addBothHandFeatures(features *[]string, parent *Word, child *Word) {
 	)
 }
 
-
-func AddUnigramFeatures(features *[]string, state *State, idx int) {
-	addUnigramFeatures(features, state, idx-2, "p_i-2")
-	addUnigramFeatures(features, state, idx-1, "p_i-1")
-	addUnigramFeatures(features, state, idx, "p_i")
-	addUnigramFeatures(features, state, idx+1, "p_i+1")
-	addUnigramFeatures(features, state, idx+2, "p_i+2")
-	addUnigramFeatures(features, state, idx+3, "p_i+3")
+func AddUnigramFeatures(features *[]string, state *State, actName string, idx int) {
+	addUnigramFeatures(features, state, actName, idx-2, "p_i-2")
+	addUnigramFeatures(features, state, actName, idx-1, "p_i-1")
+	addUnigramFeatures(features, state, actName, idx, "p_i")
+	addUnigramFeatures(features, state, actName, idx+1, "p_i+1")
+	addUnigramFeatures(features, state, actName, idx+2, "p_i+2")
+	addUnigramFeatures(features, state, actName, idx+3, "p_i+3")
 }
 
-
-func extractAttachLeftFeatures(state *State, idx int) []string {
+func extractFeatures(state *State, actName string, idx int) []string {
 	features := make([]string, 0)
-	AddUnigramFeatures(&features, state, idx)
+	AddUnigramFeatures(&features, state, actName, idx)
 	parent := state.pending[idx]
 	child := state.pending[idx+1]
 	addBothHandFeatures(&features, parent, child)
 	return features
 }
 
-func extractAttachRightFeatures(state *State, idx int) []string {
-	features := make([]string, 0)
-	AddUnigramFeatures(&features, state, idx)
-	parent := state.pending[idx+1]
-	child := state.pending[idx]
-	addBothHandFeatures(&features, parent, child)
-	return features
-}
-
 func ExtractFeatures(state *State, pair ActionIndexPair) ([]string, error) {
-	if reflect.ValueOf(pair.action).Pointer() == reflect.ValueOf(AttachLeft).Pointer() {
-		return extractAttachLeftFeatures(state, pair.index), nil
-	} else if reflect.ValueOf(pair.action).Pointer() == reflect.ValueOf(AttachRight).Pointer() {
-		return extractAttachRightFeatures(state, pair.index), nil
-	} else {
-		return nil, errors.New("wrong action")
-	}
+	actName := runtime.FuncForPC(reflect.ValueOf(pair.action).Pointer()).Name()
+	return extractFeatures(state, actName, pair.index), nil
 }
