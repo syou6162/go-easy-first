5e58f90ce457fb329b64ccd50f835322e14e5fa7
Yasuhisa Yoshida <syou6162@gmail.com>
2017-04-11 20:55:15 +0900

    testもインターフェイス合わせる

--- feature_test.go
+++ feature_test.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestAddOneHandFeatures(t *testing.T) {
+	words := make([]*Word, 0)
+	words = append(words,
+		makeRootWord(),
+		makeWord("ms.", "NNP", 1, 2),
+		makeWord("hang", "NNP", 2, 3),
+		makeWord("plays", "VBZ", 3, 0),
+		makeWord("elianti", "NNP", 4, 3),
+		makeWord(".", ".", 5, 3),
+	)
+	s := NewState(words)
 	features := make([]string, 0)
-	word := makeWord("plays", "VBZ", 3, 0)
-	addOneHandFeatures(&features, word, RIGHT)
+	addOneHandFeatures(&features, s, 1, "p_i")
 
 	if len(features) == 0 {
 		t.Error("length of features must be greater than 0")
