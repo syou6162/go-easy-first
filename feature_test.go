34d56a99bc56a8f36d870dfbd705a13c88db1da3
Yasuhisa Yoshida <syou6162@gmail.com>
2017-04-11 21:37:02 +0900

    fix test

--- feature_test.go
+++ feature_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestAddOneHandFeatures(t *testing.T) {
+func TestAddUnigramFeatures(t *testing.T) {
 	words := make([]*Word, 0)
 	words = append(words,
 		makeRootWord(),
@@ -16,42 +16,8 @@ func TestAddOneHandFeatures(t *testing.T) {
 	)
 	s := NewState(words)
 	features := make([]string, 0)
-	addUnigramFeatures(&features, s, 1, "p_i")
+	AddUnigramFeatures(&features, s, "left", 1)
 
-	if len(features) == 0 {
-		t.Error("length of features must be greater than 0")
-	}
-}
-
-func TestExtractAttachLeftFeature(t *testing.T) {
-	words := make([]*Word, 0)
-	words = append(words,
-		makeRootWord(),
-		makeWord("ms.", "NNP", 1, 2),
-		makeWord("hang", "NNP", 2, 3),
-		makeWord("plays", "VBZ", 3, 0),
-		makeWord("elianti", "NNP", 4, 3),
-		makeWord(".", ".", 5, 3),
-	)
-	s := NewState(words)
-	features := extractAttachLeftFeatures(s, 2)
-	if len(features) == 0 {
-		t.Error("length of features must be greater than 0")
-	}
-}
-
-func TestExtractAttachRightFeature(t *testing.T) {
-	words := make([]*Word, 0)
-	words = append(words,
-		makeRootWord(),
-		makeWord("ms.", "NNP", 1, 2),
-		makeWord("hang", "NNP", 2, 3),
-		makeWord("plays", "VBZ", 3, 0),
-		makeWord("elianti", "NNP", 4, 3),
-		makeWord(".", ".", 5, 3),
-	)
-	s := NewState(words)
-	features := extractAttachRightFeatures(s, 2)
 	if len(features) == 0 {
 		t.Error("length of features must be greater than 0")
 	}
