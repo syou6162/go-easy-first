4d1a114a5b6d823f649cc4a78bcffef705ee5aa0
Yasuhisa Yoshida <syou6162@gmail.com>
2017-04-22 07:16:54 +0900

    学習と評価を分けてやる

--- main.go
+++ main.go
@@ -30,26 +30,26 @@ func main() {
 		sentences = append(sentences, s)
 	}
 
+	goldSents := make([]*Sentence, 0)
+	devSents := make([]*Sentence, 0)
+	for i := 0; i < len(sentences) - 1; i++ {
+		if i < int(float64(len(sentences)) * 0.8) {
+			goldSents = append(goldSents, sentences[i])
+		} else {
+			devSents = append(devSents, sentences[i])
+		}
+	}
+
 	model := Model{make(map[string]float64), make(map[string]float64), 1}
 
 	for iter := 0; iter < 10; iter++ {
-		shuffle(sentences)
-		goldHeads := make([][]int, 0)
-		for _, sent := range sentences {
-			goldHeads = append(goldHeads, sent.ExtractHeads())
-		}
-
-		for _, sent := range sentences {
+		shuffle(goldSents)
+		for _, sent := range goldSents {
 			model.Update(sent)
 		}
 
-		predHeads := make([][]int, 0)
-		w := model.AveragedWeight()
-		for _, sent := range sentences {
-			Decode(&w, sent)
-			predHeads = append(predHeads, sent.ExtractPredictedHeads())
-		}
-		accuracy, _ := DependencyAccuracy(goldHeads, predHeads)
-		fmt.Println(accuracy)
+		trainAccuracy := DependencyAccuracy(&model, goldSents)
+		devAccuracy := DependencyAccuracy(&model, devSents)
+		fmt.Println(fmt.Sprintf("%d, %0.03f, %0.03f", iter, trainAccuracy, devAccuracy))
 	}
 }
